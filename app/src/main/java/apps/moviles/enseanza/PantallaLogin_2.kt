1de6fa9ae3c83b46e22ae2ccbfcc65ea73f82d42
Jesus Murguia <jesusmurguiafac@gmail.com>
2021-03-31 16:12:40 -0700

    funcionalidad de navegacion en las pantallas de login y registro

--- app/src/main/java/apps/moviles/enseanza/PantallaLogin_2.kt
+++ app/src/main/java/apps/moviles/enseanza/PantallaLogin_2.kt
@@ -1,11 +1,17 @@
 package apps.moviles.enseanza
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_pantalla_login.*
 
 class PantallaLogin_2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_login_2)
+
+        btnRegistro.setOnClickListener(){
+            startActivity(Intent(this, PantallaRegistrate::class.java))
+        }
     }
 }
