38299766ad17d94b288b4098df86961c3141b53a
Jesus Murguia <jesusmurguiafac@gmail.com>
2021-03-31 15:59:00 -0700

    pantalla ingresar

---
+++ app/src/main/java/apps/moviles/enseanza/PantallaLogin_2.kt
@@ -0,0 +1,11 @@
+package apps.moviles.enseanza
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PantallaLogin_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_login_2)
+    }
+}
